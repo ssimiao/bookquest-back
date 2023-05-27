1a66d3b4ef4117e0f93c52c52254b03bfa8c061c
Samuel Simião <67723030+ssimiao@users.noreply.github.com>
2023-05-27 05:32:12 -0300

    Update BookquestApplicationTests.java

--- src/test/java/io/bookquest/BookquestApplicationTests.java
+++ src/test/java/io/bookquest/BookquestApplicationTests.java
@@ -3,10 +3,8 @@ package io.bookquest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BookquestApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
