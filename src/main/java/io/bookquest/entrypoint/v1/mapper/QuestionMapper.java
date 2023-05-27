8e072fdf4139373740faa9dd3d06e27aacd53a8d
Samuel Simião <67723030+ssimiao@users.noreply.github.com>
2023-05-27 07:06:23 -0300

    Update QuestionMapper.java

--- src/main/java/io/bookquest/entrypoint/v1/mapper/QuestionMapper.java
+++ src/main/java/io/bookquest/entrypoint/v1/mapper/QuestionMapper.java
@@ -13,22 +13,6 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class QuestionMapper {
 
-    public static List<BookQuestions> toEntityList(List<BookQuizEntrypoint> dtos, Book book) {
-        return dtos.stream().map(dto -> toEntity(dto, book)).toList();
-    }
-
-    public static BookQuestions toEntity(BookQuizEntrypoint dto, Book book) {
-        return new BookQuestions(dto.getQuestion(), dto.getOptions(), dto.getCorrectAnswer(), book);
-    }
-
-    public static List<BookQuizEntrypoint> toDtoList(List<BookQuestions> dtos) {
-        return dtos.stream().map(QuestionMapper::toDto).toList();
-    }
-
-    public static BookQuizEntrypoint toDto(BookQuestions dto) {
-        return new BookQuizEntrypoint(dto.getQuestion(), dto.getOptions(), dto.getCorrectAnswer());
-    }
-
     public static List<Record> toRecord(List<BookQuizEntrypoint> bookQuestions,
                                         BookDataTransfer book) {
         return bookQuestions.stream()
